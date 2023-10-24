224c39c9991fbcb4b7ac4ad50c568711bbce0036
Anchu <chuan14402@gmail.com>
2023-10-24 13:12:17 +0700

    final

--- backend/src/main/java/com/server/backend/services/AuthenticationService.java
+++ backend/src/main/java/com/server/backend/services/AuthenticationService.java
@@ -189,9 +189,9 @@ public class AuthenticationService {
         String subject = "Xác thực tài khoản của bạn";
         String content = "Kính gửi [[name]],<br><br>"
                 + "Cảm ơn bạn vì đã dành sự quan tâm và tin tưởng tới dịch vụ của chúng tôi.<br>"
-                + "Trải nghiệm của khách hàng chính là động lực to lớn nhất của chùng tôi và nhất định chúng tôi sẽ mang đến cho bạn trải nghiệm tốt nhất. "
+                + "Trải nghiệm của khách hàng chính là động lực to lớn nhất của chúng tôi và nhất định chúng tôi sẽ mang đến cho bạn trải nghiệm tốt nhất. "
                 + "Chúng tôi luôn nỗ lực không ngừng, hướng tới mục tiêu phát triển bền vững, chú trọng xây dựng các chính sách chăm sóc khách hàng, mang lại những giá trị thiết thực để luôn làm hài lòng khách hàng ở mức cao nhất nhằm đáp lại tình cảm và sự tin yêu của bạn.<br>"
-                + "Một lần nữa cảm ởn bạn và đừng quên xác nhận tài khoản của bạn bằng link bên dưới để trải nghiệm dịch vụ:<br>"
+                + "Một lần nữa cảm ơn bạn và đừng quên xác nhận tài khoản của bạn bằng link bên dưới để trải nghiệm dịch vụ:<br>"
                 + "<h3><a href=\"[[URL]]\" target=\"_self\">Trải nghiệm dịch vụ tại đây</a></h3>"
                 + "Thân ái,<br>"
                 + "DevChu Company.";
